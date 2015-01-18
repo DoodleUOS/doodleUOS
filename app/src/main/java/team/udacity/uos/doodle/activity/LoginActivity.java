4b0635f4a2051db961d992c984341a343ac75a18
JYPark88 <hiphopgn@naver.com>
2015-01-19 00:35:07 +0900

    facebook login

---
+++ app/src/main/java/team/udacity/uos/doodle/activity/LoginActivity.java
@@ -0,0 +1,115 @@
+package team.udacity.uos.doodle.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import team.udacity.uos.doodle.R;
+
+public class LoginActivity extends Activity {
+    private UiLifecycleHelper uiHelper;
+
+    @InjectView(R.id.login_facebook_btn)
+    LoginButton mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
+
+        ButterKnife.inject(this);
+
+        Session session = Session.getActiveSession();
+        if (session != null && session.isOpened()) { // 세션이 있을 경우
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else { // 아닐 경우
+
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
+        if (session != null && session.isOpened()) {
+            makeMeRequest(session);
+        }
+    }
+
+    private void makeMeRequest(final Session session) {
+        Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+            @Override
+            public void onCompleted(GraphUser user, Response response) {
+                if (session == Session.getActiveSession()) {
+                    if (user != null) {
+                        // myId = user.getId();
+                        // myName = user.getName();
+                        // myLink = user.getLink();
+
+                        // 회원가입 이부분에서 진행하기
+
+                        String result = user.getId() + "\n" + user.getName() + "\n" + user.getLink();
+                        Toast.makeText(getBaseContext(), result, Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+                if (response.getError() != null) {
+                    // Handle errors, will do so later.
+                }
+            }
+        });
+        request.executeAsync();
+    }
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+
+}
