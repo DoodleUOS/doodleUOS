f2bda2316c2d01325ff10c047f81f81bcf3696dd
Jo <jih9734@naver.com>
2015-01-31 19:19:16 +0900

    map Activity

--- app/src/main/java/team/udacity/uos/doodle/activity/MapActivity.java
+++ app/src/main/java/team/udacity/uos/doodle/activity/MapActivity.java
@@ -1,5 +1,6 @@
 package team.udacity.uos.doodle.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
@@ -12,8 +13,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import team.udacity.uos.doodle.R;
+import team.udacity.uos.doodle.model.Doodle;
 import team.udacity.uos.doodle.util.GpsInfo;
+import team.udacity.uos.doodle.util.Util;
 
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -24,10 +29,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private double latitude = 0;
     private double longitude = 0;
 
+    List<Doodle> doodledoodle;
+    private GoogleMap mMap;
+
     @Override
     public void onMapReady(GoogleMap map) {
 //        double latitude = 0;
 //        double longitude = 0;
+        mMap = map;
 
         gps = new GpsInfo(MapActivity.this);
         // GPS 사용유무 가져오기
@@ -36,9 +45,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             latitude = bundle.getDouble("lat");
             longitude = bundle.getDouble("long");
 
-            if(latitude == 0 && longitude == 0){        // 지정된 좌표가 없으면 현재 위치
+            if(bundle.getInt("value") == 0){        // 일반 클릭
                 latitude = gps.getLatitude();
                 longitude = gps.getLongitude();
+
+                new AsyncTask<Void, Void, List<Doodle>>(){
+
+                    @Override
+                    protected List<Doodle> doInBackground(Void... params) {
+                        List<Doodle> result = Util.getDoodles(getApplicationContext());
+                        return result;
+                    }
+
+                    @Override
+                    protected void onPostExecute(List<Doodle> list) {
+                        doodledoodle = list;
+                        mapAddMarker(mMap);
+                    }
+                }.execute();
+            } else{         // 타임라인 클릭
+                map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Here!!"));
             }
         } else {
             // GPS 를 사용할수 없으므로
@@ -46,7 +72,15 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         }
 
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 16));
-        map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Here!!"));
+//        map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Here!!"));
+
+    }
+
+    private void mapAddMarker(GoogleMap map){
+        for (Doodle item : doodledoodle){
+            map.addMarker(new MarkerOptions().position(new LatLng(item.getDooLat(), item.getDooLong())).title("Here!!"));
+            System.out.println("// " + item.getDooNo() + " // " + item.getDooMemNo());
+        }
     }
 
     @Override
